a119331468f970d9b9e9984ac17f9f4043e28b4e
Mikee <islasmiguel737@gmail.com>
2025-06-03 13:05:24 -0600

    01 Models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,22 @@
+import uuid
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, max_length=255,not_null=True)
+    password_hash = models.CharField(max_length=255, not_null=True)
+    username = models.CharField(max_length=50, unique=True, not_null=True)
+    real_name = models.CharField(max_length=100, not_null=True)
+    avatar_url = models.URLField(max_length=500, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    is_email_verified = models.BooleanField(default=False)
+    email_verification_token = models.CharField(max_length=255, blank=True, null=True)
+    email_verification_expires_at = models.DateTimeField(blank=True, null=True)
+    password_reset_token = models.CharField(max_length=255, blank=True, null=True)
+    password_reset_expires_at = models.DateTimeField(blank=True, null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+
