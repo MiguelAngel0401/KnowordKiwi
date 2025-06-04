b9f147ec694f87408f7337f3b4cedc1aab32d87f
heric-hh <erhh.015@gmail.com>
2025-06-03 23:26:24 -0600

    feat: mejora en el diseño de la pantalla de login

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -4,15 +4,27 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    // <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-    //   {children}
-    // </div>
-    <div className="w-full">
-      {" "}
-      <nav className="border border-red-300">
-        <h1>KnoWord</h1>
+    <div className="w-full min-h-screen flex flex-col">
+      <nav className="w-full inline-flex items-center px-5 py-3">
+        <h1 className="text-xl font-bold mr-auto">KnoWord</h1>
+        <ul className="flex space-x-4">
+          <li>
+            <button className="px-4 py-2 bg-secondary text-color-text font-bold rounded-xl hover:bg-secondary-hover transition duration-300">
+              Registrate
+            </button>
+          </li>
+        </ul>
       </nav>
-      <div>{children}</div>
+      <main className="flex-1 flex items-center justify-center text-color-text">
+        {children}
+      </main>
+      <footer className="w-full bg-gray-900 text-color-text text-center p-8 mt-8 flex text-sm font-light">
+        <p className="">© 2023 KnoWord || Un trabajo de AlphaTech 🦙</p>
+        <div className="flex space-x-4 ml-auto">
+          <p>Términos y condiciones</p>
+          <p>Politica de privacidad</p>
+        </div>
+      </footer>
     </div>
   );
 }
--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,3 +1,54 @@
-export default function page() {
-  return <h1 className="text-3xl text-red-400">Login</h1>;
+import {
+  Button,
+  Field,
+  Fieldset,
+  Input,
+  Label,
+  Legend,
+} from "@headlessui/react";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <Fieldset className="space-y-8 bg-gray-900 rounded-lg shadow-lg max-w-lg p-8">
+      <Legend className="text-3xl font-bold text-center">
+        Inicia sesión en tu cuenta
+      </Legend>
+      <Field>
+        <h3 className="font-light text-center mb-4 text-gray-300">
+          Aprende, comparte y crece junto a una comunidad que ama el
+          conocimiento.
+        </h3>
+        <Label className="block text-sm font-medium">Correo Electrónico</Label>
+        <Input
+          type="email"
+          autoComplete="email"
+          required
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm transition duration-150 ease-in-out"
+        />
+      </Field>
+      <Field>
+        <Label className="block text-sm font-medium">Contraseña</Label>
+        <Input
+          type="password"
+          autoComplete="current-password"
+          className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-primary focus:border-primary sm:text-sm transition duration-150 ease-in-out"
+          required
+        />
+      </Field>
+      <span>¿Olvidaste tu contraseña? </span>
+      <Link
+        href="/reset-password"
+        className="text-primary hover:text-primary-hover transition duration-150 ease-in-out"
+      >
+        Recupérala en segundos.
+      </Link>
+      <Button
+        type="submit"
+        className="w-full bg-primary text-white font-bold py-2 px-4 rounded-lg hover:bg-primary-hover mt-6 transition duration-300"
+      >
+        Iniciar sesión
+      </Button>
+    </Fieldset>
+  );
 }
